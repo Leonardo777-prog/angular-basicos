22a800b05367ac6e4f13aba48770e76f8bae254f
Leonardo Cano <leomaucano111@gmail.com>
2021-05-28 22:06:39 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // nombre del componente, de la etiqueta
   selector: 'app-root',
+  // direccion de la plantilla html
   templateUrl: './app.component.html',
+  // direccion de la hoja de estilos
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  title: string = 'Contador App';
 }
